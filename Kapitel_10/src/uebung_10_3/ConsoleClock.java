e4544a33cff13cee84934c2b05a63e3e2a4979a7
SuperMario4848 <34087110+SuperMario4848@users.noreply.github.com>
2021-01-13 20:11:50 +0100

    improved console_clock

--- Kapitel_10/src/uebung_10_3/ConsoleClock.java
+++ Kapitel_10/src/uebung_10_3/ConsoleClock.java
@@ -1,20 +1,19 @@
 package uebung_10_3;
 
 import java.text.SimpleDateFormat;
-import java.util.GregorianCalendar;
+import java.util.Date;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 public class ConsoleClock {
 
 	public static void main(String[] args) {
-		TimeZone timeZone = TimeZone.getTimeZone("Europe/Berlin");
-		SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE', 'dd. MMMM yyyy hh:mm:s z");
+		TimeZone timeZone = TimeZone.getTimeZone("America/New_York");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE', 'dd. MMMM yyyy HH:mm:ss z");
 		dateFormat.setTimeZone(timeZone);
 		
 		while (true) {
-			GregorianCalendar calendar = new GregorianCalendar(timeZone);
-			System.out.println(dateFormat.format(calendar.getTime()));
+			System.out.println(dateFormat.format(new Date()));
 			try {
 				TimeUnit.SECONDS.sleep(1);
 			}
