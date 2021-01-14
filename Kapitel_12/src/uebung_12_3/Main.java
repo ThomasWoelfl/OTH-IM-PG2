3ae8d42df849d7aa4bab8cebe7290ce93b0fc8b6
SuperMario4848 <34087110+SuperMario4848@users.noreply.github.com>
2021-01-14 18:17:10 +0100

    improved multithreaded llt

--- Kapitel_12/src/uebung_12_3/Main.java
+++ Kapitel_12/src/uebung_12_3/Main.java
@@ -3,109 +3,44 @@ package uebung_12_3;
 public class Main {
 
 	public static void main(String[] args) {
-		int exponent = 19;
+		int maxExponent = 86243;
 
-		int i = 3;
+		int currentExponent = 86242;
 
-		//Creating 8 new Thread. Every single one is dead, because we did not start it.
-		PrimeTest p1 = new PrimeTest(0);
-		PrimeTest p2 = new PrimeTest(0);
-		PrimeTest p3 = new PrimeTest(0);
-		PrimeTest p4 = new PrimeTest(0);
-		PrimeTest p5 = new PrimeTest(0);
-		PrimeTest p6 = new PrimeTest(0);
-		PrimeTest p7 = new PrimeTest(0);
-		PrimeTest p8 = new PrimeTest(0);
+		// Creating an array with the size of the available processors
+		PrimeTest primeTests[] = new PrimeTest[Runtime.getRuntime().availableProcessors()];
 
-		//If one of our threads has done its job, it dies. So if the thread is not alive anymore, we can create a new one with a new number to test.
+
+		// If one of our threads has done its job, it dies. So if the thread is not
+		// alive anymore, we can create a new one with a new number to test.
+		// We do this as long we have reached the max exponent.
 		while (true) {
-			if (!p1.isAlive()) {
-				p1 = new PrimeTest(i);
-				p1.start();
-				i++;
-				if (stop(i, exponent)) {
-					break;
+			for (int i = 0; i < primeTests.length; i++) {
+				if ((primeTests[i] == null || !primeTests[i].isAlive()) && currentExponent <= maxExponent) {
+					primeTests[i] = new PrimeTest(currentExponent);
+					primeTests[i].start();
+					currentExponent++;
 				}
 			}
-			if (!p2.isAlive()) {
-				p2 = new PrimeTest(i);
-				p2.start();
-				i++;
-				if (stop(i, exponent)) {
-					break;
-				}
-			}
-			if (!p3.isAlive()) {
-				p3 = new PrimeTest(i);
-				p3.start();
-				i++;
-				if (stop(i, exponent)) {
-					break;
-				}
-			}
-			if (!p4.isAlive()) {
-				p4 = new PrimeTest(i);
-				p4.start();
-				i++;
-				if (stop(i, exponent)) {
-					break;
-				}
-			}
-			if (!p5.isAlive()) {
-				p5 = new PrimeTest(i);
-				p5.start();
-				i++;
-				if (stop(i, exponent)) {
-					break;
-				}
-			}
-			if (!p6.isAlive()) {
-				p6 = new PrimeTest(i);
-				p6.start();
-				i++;
-				if (stop(i, exponent)) {
-					break;
-				}
-			}
-			if (!p7.isAlive()) {
-				p7 = new PrimeTest(i);
-				p7.start();
-				i++;
-				if (stop(i, exponent)) {
-					break;
-				}
-			}
-			if (!p8.isAlive()) {
-				p8 = new PrimeTest(i);
-				p8.start();
-				i++;
-				if (stop(i, exponent)) {
-					break;
-				}
+
+			// If the maximum exponent is reached, we want to break the while-loop.
+			if (currentExponent > maxExponent) {
+				System.out.println("reached");
+				break;
 			}
 		}
 
-		//wait for all running threads before we end the programm
+		// wait for all running threads before we end the program. Otherwise the main-thread would end before the calculation threads. Not necessary but nice.
 		try {
-			p1.join();
-			p2.join();
-			p3.join();
-			p4.join();
-			p5.join();
-			p6.join();
-			p7.join();
-			p8.join();
-		} catch (InterruptedException e) {
+			for (int i = 0; i < primeTests.length; i++) {
+				if(primeTests[i] != null) {
+					primeTests[i].join();
+				}
+			}
+		}
+		catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	//If the maximum exponent is reached, we want to break the while-loop
-	private static boolean stop(int index, int exponent) {
-		if (index > exponent) {
-			return true;
-		}
-		return false;
 	}
 }
